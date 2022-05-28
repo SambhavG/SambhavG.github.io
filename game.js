7a9e541c15a314633a757054088744210c6b95c8
Sambhav Gupta <sambhavgupta159@gmail.com>
2022-05-27 21:59:44 -0400

    Tic tac toe

--- game.js
+++ game.js
@@ -6,145 +6,108 @@ window.onload = () => {
 }
 
 function gameLoop() {
-    setInterval(show, 1000/20) // here 15 is our fps value
-}
-
-function show() {
-    update()
-    draw()
+    setInterval(update, 1000/20) // here 15 is our fps value
 }
 
 function update() {
     canvasContext.clearRect(0, 0, canvas.width, canvas.height)
-    snake.move()
-    eatApple()
-    checkHitWall()
+
+
+    grid.draw()
 }
 
-function eatApple() {
-    if(snake.tail[snake.tail.length - 1].x == apple.x &&
-        snake.tail[snake.tail.length - 1].y == apple.y){
-            snake.tail[snake.tail.length] = {x:apple.x, y: apple.y}
-            apple = new Apple();
-        }
-}
-
-function checkHitWall() {
-    let headTail = snake.tail[snake.tail.length -1]
-
-    if (headTail.x == - snake.size) {
-        headTail.x = canvas.width - snake.size
-    } else if (headTail.x == canvas.widh) {
-        headTail.x = 0
-    } else if (headTail.y == - snake.size) {
-        headTail.y = canvas.height - snake.size
-    } else if (headTail.y == canvas.height) {
-        headTail.y = 0 
+class ticTacToeGrid {
+    constructor() {
+        this.grid = [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ]
+        this.turn = 1
     }
-}
-
-function draw() {
-    createRect(0,0,canvas.width, canvas.height, "black")
-    createRect(0,0, canvas.width, canvas.height)
-
-    for (let i = 0; i < snake.tail.length; i++){
-        createRect(snake.tail[i].x + 2.5, snake.tail[i].y + 2.5,
-            snake.size - 5, snake.size- 5, "white")
-    }
-
-    canvasContext.font = "20px Arial"
-    canvasContext.fillStyle = "#00FF42"
-    canvasContext.fillText("Score: " + (snake.tail.length -1),canvas.width - 120, 18)
-    createRect(apple.x, apple.y, apple.size, apple.size, apple.color)
-}
-
-function createRect(x,y,width, height,color) {
-    canvasContext.fillStyle = color
-    canvasContext.fillRect(x, y, width, height)
-}
-
-window.addEventListener("keydown", (event) => {
-    setTimeout(() => {
-        if (event.keyCode == 37 && snake.rotateX != 1) {
-            snake.rotateX = -1
-            snake.rotateY = 0
-        } else if (event.keyCode == 38 && snake.rotateY != 1) {
-            snake.rotateX = 0
-            snake.rotateY = -1
-        } else if (event.keyCode == 39 && snake.rotateX != -1) {
-            snake.rotateX = 1
-            snake.rotateY = 0
-        } else if (event.keyCode == 40 && snake.rotateY != -1) {
-            snake.rotateX = 0
-            snake.rotateY = 1
-        }
-    }, 1)
-})
-
-class Snake {
-    constructor(x, y, size) {
-        this.x = x
-        this.y = y
-        this.size = size
-        this.tail = [{x:this.x, y:this.y}]
-        this.rotateX = 0
-        this.rotateY = 1
-    }
-
-    move() {
-        let newRect
-
-        if (this.rotateX == 1) {
-            newRect = {
-                x: this.tail[this.tail.length - 1].x + this.size,
-                y: this.tail[this.tail.length - 1].y
-            }
-        } else if (this.rotateX == -1) {
-            newRect = {
-                x: this.tail[this.tail.length - 1].x - this.size,
-                y: this.tail[this.tail.length - 1].y
-            }
-        } else if (this.rotateY == 1) {
-            newRect = {
-                x: this.tail[this.tail.length - 1].x,
-                y: this.tail[this.tail.length - 1].y + this.size
-            }
-        } else if (this.rotateY == -1) {
-            newRect = {
-                x: this.tail[this.tail.length - 1].x,
-                y: this.tail[this.tail.length - 1].y - this.size
-            }
-        }
-
-        this.tail.shift()
-        this.tail.push(newRect)
-    }
-}
-
-class Apple{
-    constructor(){
-        let isTouching
-        
-        while (true) {
-            isTouching = false;
-            this.x = Math.floor(Math.random() * canvas.width / snake.size) * snake.size
-            this.y = Math.floor(Math.random() * canvas.height / snake.size) * snake.size
-            
-            for (let i = 0; i < snake.tail.length; i++) {
-                if (this.x == snake.tail[i].x && this.y == snake.tail[i].y) {
-                    isTouching = true
+    draw() {
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (this.grid[i][j] === 1) {
+                    canvasContext.fillStyle = 'red'
+                    canvasContext.fillRect(i * 100, j * 100, 100, 100)
+                } else if (this.grid[i][j] === 2) {
+                    canvasContext.fillStyle = 'blue'
+                    canvasContext.fillRect(i * 100, j * 100, 100, 100)
+                } else {
+                    canvasContext.fillStyle = 'white'
+                    canvasContext.fillRect(i * 100, j * 100, 100, 100)
                 }
             }
-
-            this.size = snake.size
-            this.color = "red"
-
-            if (!isTouching) {
-                break;
-            }
         }
     }
+    cellClicked(x, y) {
+        if (this.grid[x][y] === 0) {
+            this.grid[x][y] = this.turn
+            this.flipTurn()
+        }
+    }
+    flipTurn() {
+        this.turn = this.turn === 1 ? 2 : 1
+    }
+    aiMove() {
+        //Until an unfilled square is found, pick random squares
+        do {
+            let x = Math.floor(Math.random() * 3)
+            let y = Math.floor(Math.random() * 3)
+        } while (this.grid[x][y] !== 0)
+        this.grid[x][y] = this.turn
+        this.flipTurn()
+    }
+    checkWin() {
+        for (let i = 0; i < 3; i++) {
+            if (this.grid[i][0] === this.grid[i][1] && this.grid[i][1] === this.grid[i][2]) {
+                return this.grid[i][0]
+            }
+        }
+        for (let i = 0; i < 3; i++) {
+            if (this.grid[0][i] === this.grid[1][i] && this.grid[1][i] === this.grid[2][i]) {
+                return this.grid[0][i]
+            }
+        }
+        if (this.grid[0][0] === this.grid[1][1] && this.grid[1][1] === this.grid[2][2]) {
+            return this.grid[0][0]
+        }
+        if (this.grid[0][2] === this.grid[1][1] && this.grid[1][1] === this.grid[2][0]) {
+            return this.grid[0][2]
+        }
+        //If no squares are empty, return 3 to indicate a draw
+        if (this.grid.flat().every(x => x !== 0)) {
+            return 3
+        }
+        return 0
+    }
+    returnResult() {
+        if (this.checkWin() === 1) {
+            alert("Red wins!")
+        } else if (this.checkWin() === 2) {
+            alert("Blue wins!")
+        } else if (this.checkWin() === 3) {
+            alert("Tie!")
+        }
+    }
+
+
+
 }
 
-const snake = new Snake(20,20,20);
-let apple = new Apple();
+
+window.addEventListener("mousedown", (event) => {
+    let mouseX = event.clientX - canvas.offsetLeft
+    let mouseY = event.clientY - canvas.offsetTop
+    let gridX = Math.floor(mouseX / (canvas.width/3))
+    let gridY = Math.floor(mouseY / (canvas.height/3))
+    if (grid.grid[gridX][gridY] === 0) {
+        grid.grid[gridX][gridY] = 1
+        grid.returnResult()
+    }
+
+})
+
+
+let grid = new ticTacToeGrid()
