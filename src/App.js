ddcaa7741a23dbc1f2d2c497e7386a3f91bec049
SambhavG <sambhavgupta159@gmail.com>
2023-08-07 09:02:14 -0700

    Parallelism

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import BackgroundCanvas from "./BackgroundCanvas";
 
 function App() {
   return (
-    <div class="App" id="app">
+    <div>
       <BackgroundCanvas class="background-canvas-component" />
       <div class="content">
         <div class="header">Sambhav Gupta</div>
 
         <div class="main-statement">
           Hello, I'm Sambhav. <br />
-          I'm a student, a software engineer, and ML enthusiast. <br />I study math, computer science, and machine
+          I'm a student, software engineer, and ML enthusiast. <br />I study math, computer science, and machine
           learning at Stanford. <br />
           I've worked at Amazon, The Amherst Group, and a contech startup. <br />
         </div>
