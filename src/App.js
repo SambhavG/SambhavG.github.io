2b5ead3dfde04a2c1bf6a4500a93889ec7d599e9
SambhavG <sambhavgupta159@gmail.com>
2023-08-07 00:03:54 -0700

    First draft of front page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import BackgroundCanvas from './BackgroundCanvas'
+import "./App.css";
+import BackgroundCanvas from "./BackgroundCanvas";
 
 function App() {
   return (
-    <div className="App">
+    <div class="App" id="app">
       <BackgroundCanvas class="background-canvas-component" />
+      <div class="content">
+        <div class="header">Sambhav Gupta</div>
 
-
+        <div class="main-statement">
+          Hello, I'm Sambhav. <br />
+          I'm a student, a software engineer, and ML enthusiast. <br />I study math, computer science, and machine
+          learning at Stanford. <br />
+          I've worked at Amazon, The Amherst Group, and a contech startup. <br />
+        </div>
+      </div>
     </div>
   );
 }
--- src/backgroundCanvasController.js
+++ src/backgroundCanvasController.js
@@ -2,6 +2,9 @@ function runCanvasAnimation(width, height) {
   var dots = [];
   var numDots = 100;
 
+  var useDotsDensity = true;
+  var dotsDensity = 1; //1 is 100 dots on regular screen
+
   var speedScale = 1;
   var repulsionForce = 20;
   var mouseCaptureDistance = 45;
@@ -9,6 +12,9 @@ function runCanvasAnimation(width, height) {
   var repulsionRange = 30;
   var drawLineRange = 70;
   var mouseAttractionForce = 1;
+
+  var lightestShade = 180;
+  var backgroundShade = 15;
 
   function dist(x1, y1, x2, y2) {
     return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
@@ -18,19 +24,18 @@ function runCanvasAnimation(width, height) {
     constructor(width, height) {
       this.x = Math.random() * width;
       this.y = Math.random() * height;
-      
+
       //this.xspeed = Math.random() * (speedScale) - speedScale/2;
       //this.yspeed = Math.random() * (speedScale) - speedScale/2;
-      
-      this.xspeed = Math.random()*2-1;
-      this.yspeed = Math.random()*2-1;
-      var mag = Math.sqrt(this.xspeed * this.xspeed + this.yspeed *   this.yspeed) / speedScale;
+
+      this.xspeed = Math.random() * 2 - 1;
+      this.yspeed = Math.random() * 2 - 1;
+      var mag = Math.sqrt(this.xspeed * this.xspeed + this.yspeed * this.yspeed) / speedScale;
       this.xspeed /= mag;
       this.yspeed /= mag;
     }
 
     update(mousex, mousey, canvasWidth, canvasHeight) {
-
       if (this.x <= 0 || this.x >= canvasWidth) {
         this.xspeed *= -1;
       }
@@ -47,17 +52,17 @@ function runCanvasAnimation(width, height) {
         var xdiff = this.x - mousex;
         var ydiff = this.y - mousey;
         var angle = Math.atan2(ydiff, xdiff);
-        xspeedVal = -mouseAttractionForce * (theDist-mouseHoldDistance) * Math.cos(angle);
-        yspeedVal = -mouseAttractionForce * (theDist-mouseHoldDistance) * Math.sin(angle);
-      
+        xspeedVal = -mouseAttractionForce * (theDist - mouseHoldDistance) * Math.cos(angle);
+        yspeedVal = -mouseAttractionForce * (theDist - mouseHoldDistance) * Math.sin(angle);
 
         for (var i = 0; i < dots.length; i++) {
           if (dots[i] !== this) {
             var d = dist(this.x, this.y, dots[i].x, dots[i].y);
-            if (d < repulsionRange) { // Adjust the repulsion range as needed
+            if (d < repulsionRange) {
+              // Adjust the repulsion range as needed
               var angle2 = Math.atan2(this.y - dots[i].y, this.x - dots[i].x);
-              xspeedVal += repulsionForce * Math.cos(angle2) / (d + 1);
-              yspeedVal += repulsionForce * Math.sin(angle2) / (d + 1);
+              xspeedVal += (repulsionForce * Math.cos(angle2)) / (d + 1);
+              yspeedVal += (repulsionForce * Math.sin(angle2)) / (d + 1);
             }
           }
         }
@@ -68,11 +73,10 @@ function runCanvasAnimation(width, height) {
     }
 
     display(canvasInput) {
-      canvasInput.strokeStyle = 'black';
-      canvasInput.fillStyle = 'lightgray';
+      //Dot style is a filled circle with no outline
+      canvasInput.fillStyle = `rgb(${lightestShade}, ${lightestShade}, ${lightestShade})`;
       canvasInput.beginPath();
-      canvasInput.arc(this.x, this.y, 2, 0, Math.PI * 2);
-      canvasInput.stroke();
+      canvasInput.arc(this.x, this.y, 1, 0, 2 * Math.PI);
       canvasInput.fill();
       canvasInput.closePath();
     }
@@ -83,6 +87,9 @@ function runCanvasAnimation(width, height) {
     var canvasHeight = canvasInput.canvas.height;
 
     canvasInput.clearRect(0, 0, canvasWidth, canvasHeight);
+    //(30, 30, 30) background
+    canvasInput.fillStyle = `rgb(${backgroundShade}, ${backgroundShade}, ${backgroundShade})`;
+    canvasInput.fillRect(0, 0, canvasWidth, canvasHeight);
 
     for (var i = 0; i < dots.length; i++) {
       dots[i].update(mousex, mousey, canvasWidth, canvasHeight);
@@ -91,13 +98,21 @@ function runCanvasAnimation(width, height) {
     displayLines(dots, canvasInput);
   }
 
+  function map(value, oldRange, newRange) {
+    var newValue = ((value - oldRange[0]) * (newRange[1] - newRange[0])) / (oldRange[1] - oldRange[0]) + newRange[0];
+    return Math.min(Math.max(newValue, newRange[0]), newRange[1]);
+  }
+
   function displayLines(dots, canvasInput) {
     for (var i = 0; i < dots.length; i++) {
       for (var j = i + 1; j < dots.length; j++) {
-        if (dist(dots[i].x, dots[i].y, dots[j].x, dots[j].y) < drawLineRange) {
-          canvasInput.strokeStyle = `rgba(0, 0, 0, ${
-            1 - dist(dots[i].x, dots[i].y, dots[j].x, dots[j].y) / drawLineRange
-          })`;
+        var dotsDist = dist(dots[i].x, dots[i].y, dots[j].x, dots[j].y);
+        if (dotsDist < drawLineRange) {
+          //Lines are pure white if close, more gray or black as they are further away
+          var grayVal = map(-dotsDist, [-drawLineRange, 0], [backgroundShade, lightestShade]);
+          //var grayVal = Math.floor(255 * (1 - dist(dots[i].x, dots[i].y, dots[j].x, dots[j].y) / drawLineRange));
+          canvasInput.strokeStyle = "rgb(" + grayVal + "," + grayVal + "," + grayVal + ")";
+          //canvasInput.strokeStyle = `rgba(0, 0, 0, ${1 - dist(dots[i].x, dots[i].y, dots[j].x, dots[j].y) / drawLineRange})`;
           canvasInput.beginPath();
           canvasInput.moveTo(dots[i].x, dots[i].y);
           canvasInput.lineTo(dots[j].x, dots[j].y);
@@ -111,16 +126,14 @@ function runCanvasAnimation(width, height) {
   var mouseX = 0;
   var mouseY = 0;
 
-  var canvas = document.getElementById('background-canvas');
-  var ctx = canvas.getContext('2d');
+  var canvas = document.getElementById("background-canvas");
+  var app = document.getElementById("app");
+  var ctx = canvas.getContext("2d");
 
-  canvas.addEventListener('mousemove', function (e) {
+  app.addEventListener("mousemove", function (e) {
     var cRect = canvas.getBoundingClientRect();
-    var scale = 1
-    mouseX = Math.round(e.clientX - cRect.left)/scale;
-    mouseY = Math.round(e.clientY - cRect.top)/scale;
-    console.log(mouseX);
-    
+    mouseX = Math.round(e.clientX - cRect.left);
+    mouseY = Math.round(e.clientY - cRect.top);
   });
 
   function draw() {
@@ -134,7 +147,12 @@ function runCanvasAnimation(width, height) {
     canvas.width = width;
     canvas.height = height;
 
-    for (var i = 0; i < numDots; i++) {
+    var numDotsToUse = numDots;
+    if (useDotsDensity) {
+      numDotsToUse = Math.floor((canvas.width * canvas.height) / 15000 / dotsDensity);
+    }
+
+    for (var i = 0; i < numDotsToUse; i++) {
       dots[i] = new Dot(canvas.width, canvas.height);
     }
 
@@ -142,7 +160,6 @@ function runCanvasAnimation(width, height) {
   }
 
   setup();
-
 }
 
 export default runCanvasAnimation;
